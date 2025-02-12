765b0fac828b6d55ae0ac77da2dfb515a1813128
Kushagra Singh Bisen <sendtokush@icloud.com>
2025-02-12 15:22:03 +0100

    updated hosts

--- packages/uma/bin/main.ts
+++ packages/uma/bin/main.ts
@@ -5,7 +5,7 @@ import { setLogger } from '../src/util/logging/LoggerUtils';
 import { WinstonLogger } from '../src/util/logging/WinstonLogger';
 
 const protocol = 'http';
-const host = 'localhost';
+const host = 'n063-02b.wall2.ilabt.iminds.be';
 const port = 4000;
 
 const baseUrl = `${protocol}://${host}:${port}/uma`;
